803e00a70aa6e735502f33154c2d0b72b3057448
khaledbouguerra <khaledbouguerra92@gmail.com>
2016-07-23 04:47:27 +0100

    contact page added,Entity contact added , validators added , blog.css added, function contactAction

--- src/BlogBundle/Controller/PageController.php
+++ src/BlogBundle/Controller/PageController.php
@@ -2,6 +2,8 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\Enquiry;
+use BlogBundle\Form\EnquiryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
@@ -14,5 +16,27 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('BlogBundle:Page:about.html.twig');
+    }
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
